6abe3571ea696a95adb57f76b6443e40bb6b838c
dmoritz <daniel.moritz@trivago.com>
2018-05-25 15:21:48 +0200

    first glance at comic details

--- src/Controller/ComicController.php
+++ src/Controller/ComicController.php
@@ -8,18 +8,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ComicController extends Controller
 {
+
     /**
+     * @param $comic_id
+     * @return \Symfony\Component\HttpFoundation\Response
      * @Route("/comic/details/{comic_id}", name="comic")
      */
     public function index($comic_id)
     {
+        /** @var Comic $comic */
         $comic = $this->getDoctrine()
             ->getRepository(Comic::class)
             ->find($comic_id);
 
         return $this->render('comic/details.html.twig', [
             'controller_name' => 'ComicController',
-            'comic'           => $comic
+            'comic'           => $comic,
         ]);
     }
 }
